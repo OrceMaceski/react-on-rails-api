69bc9ad0eb3a1a58441d539a3bdc28d64a754af4
orcemaceski <maceskiorce@gmail.com>
2025-03-02 23:13:59 +0100

    Handle exceptions for authenticationg with JWT in applicaiton controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,26 +1,24 @@
 class ApplicationController < ActionController::API
+  # include JwtAuthentication
   include ActionController::MimeResponds
   respond_to :json
 
   before_action :authenticate_user_from_token!
+
+  rescue_from JWT::DecodeError, with: :handle_jwt_error
+  rescue_from ActiveRecord::RecordNotFound, with: :handle_record_not_found
 
   private
 
   def authenticate_user_from_token!
     return unless request.headers["Authorization"].present?
 
-    begin
-      jwt_payload = JWT.decode(
-        request.headers["Authorization"].split.last,
-        Rails.application.credentials.devise_jwt_secret_key!
-      ).first
+    jwt_payload = JWT.decode(
+      request.headers["Authorization"].split.last,
+      Rails.application.credentials.devise_jwt_secret_key!
+    ).first
 
-      @current_user = User.find(jwt_payload["sub"])
-    rescue JWT::DecodeError => e
-      render json: { error: "Invalid token", details: e.message  }, status: :unauthorized
-    rescue ActiveRecord::RecordNotFound
-      render json: { error: "User not found" }, status: :unauthorized
-    end
+    @current_user = User.find(jwt_payload["sub"])
   end
 
   def current_user
@@ -29,5 +27,13 @@ class ApplicationController < ActionController::API
 
   def authenticate_user!
     render json: { error: "Not authenticated" }, status: :unauthorized unless current_user
+  end
+
+  def handle_jwt_error(exception)
+    render json: { error: "Invalid token", details: exception.message }, status: :unauthorized
+  end
+
+  def handle_record_not_found
+    render json: { error: "User not found" }, status: :unauthorized
   end
 end

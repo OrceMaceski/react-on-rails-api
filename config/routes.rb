68e7c573bd2188f9effc8b7d88eeeb6abdc921b2
orcemaceski <maceskiorce@gmail.com>
2025-03-16 00:31:40 +0100

    ok status on root route, favicon 204 status

--- config/routes.rb
+++ config/routes.rb
@@ -13,4 +13,7 @@ Rails.application.routes.draw do
   defaults: { format: :json }
 
   get "up" => "rails/health#show", as: :rails_health_check
+  get "/favicon.ico", to: proc { [ 204, {}, [] ] }
+
+  root to: proc { [ 200, { "Content-Type" => "application/json" }, [ { status: "ok" }.to_json ] ] }
 end
